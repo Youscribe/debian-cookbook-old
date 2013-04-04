ff5b1387fceb46926b042b559a1460d36d04e875
Guilhem Lettron <guilhem@lettron.fr>
2013-04-04 15:03:07 +0200

    release v0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ name             "debian"
 license          "Apache 2.0"
 description      "Sets up sources for debian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 depends          "apt"
 supports         "debian"
 recipe           "debian", "Sets up sources for the node's debian release"
